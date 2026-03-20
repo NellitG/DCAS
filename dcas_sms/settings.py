0dbcd2f175c3fdf7bcf575335c85b05612c8a67e
NellitG <kamaunelson157@gmail.com>
2026-03-21 00:01:05 +0300

    remove the double registrations of analytics app and make migrations

--- dcas_sms/settings.py
+++ dcas_sms/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'farmers',
-    'messaging',
     'campaigns',
-    'analytics',
+    'messaging',
     'integrations',
     'analytics',
 ]
