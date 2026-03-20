85621035a1cfc439cd74cace695d081a8d7cd481
NellitG <kamaunelson157@gmail.com>
2026-03-21 00:50:26 +0300

    create logic to retrieve message and status

--- integrations/views.py
+++ integrations/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from messaging.models import Message
+from django.utils import timezone
 
-# Create your views here.
+@api_view(["POST"])
+def delivery_callback(request):
+    external_id = request.data.get("message_id")
+    status = request.data.get("status")
+
+    message = Message.objects.get(external_id=external_id)
+    message.status = status
+
+    if status == "delivered":
+        message.delivered_at = timezone.now()
+
+    message.save()
+
+    return Response({"status": "ok"})
