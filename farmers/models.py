a8273efbeb0fb438324bbbe2a921255821f3b3b3
NellitG <kamaunelson157@gmail.com>
2026-03-20 23:47:14 +0300

    create model for farmers

--- farmers/models.py
+++ farmers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Farmer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    county = models.CharField(max_length=100)
+    sub_county = models.CharField(max_length=100)
+    ward = models.CharField(max_length=100)
+
+    value_chain = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
