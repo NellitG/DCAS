bdb6f932ff7316820c15d1cfdfc42fdb36e26484
NellitG <kamaunelson157@gmail.com>
2026-03-20 23:56:59 +0300

    create model for campaigns

--- campaigns/models.py
+++ campaigns/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Campaign(models.Model):
+    name = models.CharField(max_length=255)
+    advisory_type = models.CharField(max_length=50)
+
+    scheduled_at = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=50, default="pending")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
