5040e642ee21df53fc08ecbb1c33faefd551f19c
NellitG <kamaunelson157@gmail.com>
2026-04-01 12:23:05 +0300

    Add sms function to send sms messages via API

---
+++ integrations/sms.py
@@ -0,0 +1,13 @@
+# integrations/sms.py
+import requests
+
+API_URL = ""
+
+def send_sms(phone, text):
+    payload = {
+        "to": phone,
+        "message": text
+    }
+
+    response = requests.postcls(API_URL, json=payload)
+    return response.json()
